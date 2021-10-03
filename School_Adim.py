c6ef5c8e0d5e8e1628af17ff76e6ffe8f0c9f7de
Harish024 <91045615+Harish024@users.noreply.github.com>
2021-10-03 16:00:01 +0530

    Update School_Adim.py

--- School_Adim.py
+++ School_Adim.py
@@ -1,13 +1,20 @@
+import csv
+def write_into_csv(info_list):
+    with open('student_info.csv', 'w+', new line='') as csv_file:
+        writer = csv.writer(csv_file)
 
-condition = True
+        writer.writerow(info_list)
+
+   condition = True
 
 while(condition):
-    student_info = input("Enter the student Register Roll Number , Name , Age , Email , Mobile :")
-    
-    spliting_list = student_info.split(' ')
-    print("spited new info : " + str(spliting_list))
-    
-    print(student_info
+    student_info = input("Enter student information in the following format (Name Age Contact Number E-mail_ID): ")
+    print("Entered information:" + student_info)
+
+    student_info_list = student_info.split(' ')
+    print("Entered split up information is:" + str(student_info_list))
+
+    write_into_csv (student_info_list)
     
     condition_cheak = input( "Enter to countinue with others (yes / no)")
     if condition_cheak == "yes":
